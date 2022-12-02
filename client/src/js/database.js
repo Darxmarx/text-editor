7b65b8950ecf79654af7b1241d85d4f00605ff2b
Darx <mcrivabode@gmail.com>
2022-12-01 17:26:54 -0800

    progress on putDb for database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (id, content) => {
+  // log the attempt to PUT/update data in the db
+  console.log('PUT to the DB!');
+
+  // create connection to the DB and its version
+  const jateDb = await openDB('jate', 1);
+
+  // create new transaction (tx, not Texas) and specify db and data privileges to write over data
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  // open up object store
+  const store = tx.objectStore('jate');
+
+  // update data in the db with new content
+  const request = store.put({ id: id, value: content });
+
+  // confirmation of request
+  const result = await request;
+  console.log('Data Updated: ', result);
+  return result;
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
@@ -23,8 +43,8 @@ export const getDb = async () => {
   // create connection to the DB and its version
   const jateDb = await openDB('jate', 1);
 
-  // create new transaction (tx, not Texas) and specify db and data privileges
-  const tx = jateDb.transaction('contact', 'readonly');
+  // create new transaction (tx, not Texas) and specify db and data privileges to only view data
+  const tx = jateDb.transaction('jate', 'readonly');
 
   // open up object store
   const store = tx.objectStore('jate');
@@ -34,7 +54,7 @@ export const getDb = async () => {
 
   // confirmation of request
   const result = await request;
-  console.log('result.value: ', result);
+  console.log('Data Received: ', result);
   return result;
 }
 
